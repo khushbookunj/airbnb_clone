5ed3367af2c2dfeadf1346579dd35d6d64e4076d
Sharique9199 <mdshariqueraj8292@gmail.com>
2023-12-22 21:33:22 +0530

    Card section by sharique

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Layout from '../../Layout'
-import Card from '../../components/Card/Card'
+import Card from '../../components/Card/Card';
+import cardJson from '../../wharehouse/Lake.json';
+import PageContainer from '../../components/PageContainer';
 
 const HomePage = () => {
+  console.log(cardJson);
   return (
     <>
       <Layout>
-        <Card/>
+        <PageContainer data={cardJson?.data} />
       </Layout>
     </>
   )
