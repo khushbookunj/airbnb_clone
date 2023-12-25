ded480d757c0338b77407697e63f8c5d7e062028
Sharique9199 <mdshariqueraj8292@gmail.com>
2023-12-25 21:01:58 +0530

    imgContiner fixed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
    
      <>
-     <Routes>
+     <Routes >
       <Route path="/" element={ <HomePage/>}/>
       <Route path='/hotel-detailPage/:name' element={<HotelDetails />} />
      </Routes>
--- src/components/HotelDetailsPage/HotelDtailImage.jsx
+++ src/components/HotelDetailsPage/HotelDtailImage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './HotelDetailPage.module.css';
 const HotelDtailImage = ({image}) => {
+  console.log("Sharique",image[0]);
+ 
+ 
+  // console.log("MainImg",MainImg[0]);
     // console.log(props)
     console.log(image)
   return (
@@ -9,24 +13,15 @@ const HotelDtailImage = ({image}) => {
             <div className={styles.image__container}>
    
             <div className={styles.lower__image}>
-             { image.map((item,index)=>{
-              return console.log(item);
-              <img src={item[0]} />
-             }
-
-             )
-             }
+             <img src={image[0]}  />
                 {/* <img src='https://a0.muscache.com/im/pictures/miso/Hosting-837315422629442025/original/dd6e35a7-eeaa-4144-8c66-b05936ba1c77.jpeg' /> */}
             </div>
             <div className={styles.lower__image1}>
             {
-        image.map((item,index)=>(
-           
+           image.slice(1).map((item)=>(
                 <img src={item}/>
-           
-            
         ))
-    }
+      }
      </div>
     </div>
    
